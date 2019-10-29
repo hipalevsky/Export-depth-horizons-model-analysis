adde99d0325b81ad4b7137a95669ba9b267288a3
Hilary Palevsky <hpalevsky@whoi.edu>
2019-10-29 16:34:28 -0400

    Completed implementation of new approach to calculate global Taylor decomposition accounting for change in MLD_max criterion
    Uses new criterion of MLD_max_2005-2024 to decompose changes over 21st century, and then visualizes this version of decomposition compared with earlier approach to the MLD_max_change term

--- CSMBGC_RCP8_5_TaylorDecomposition.m
+++ CSMBGC_RCP8_5_TaylorDecomposition.m
@@ -1,14 +1,32 @@
 %% Taylor decomposition of influences on POC flux change over the 21st century
 % Run after CSMBGC_RCP8_5_changeOverTime
 
+% This script calculates the Taylor decomposition at both the fixed 100 m depth
+% horizon (as was done in Laufkotter et al 2016) and at the MLD_max
+% horizon, following three different methods:
+% 1) Wrapping all of the MLD_max change into the E-Ratio term,
+% 2) Calculating NPP and E-Ratio terms at the fixed 100 m depth horizon and 
+    % then adding a third term to account for POC flux change due to change
+    % in MLD, using the global mean POC flux curve to estimate the influence,
+% 3) Calculating NPP and E-Ratio terms at the climatological MLD_max from 
+    % the beginning of the century (2005-2024) and adding a third terms to 
+    % account for POC flux change due to change in MLD by taking the 
+    % difference between the actual POC flux at the 2081-2100 MLD_max and
+    % the POC flux from those same years but instead determined at the
+    % climatological MLD_max from 2005-2024.
+
 %% Load in climatological annual mean POC flux and NPP in 3D (lat, lon, depth) for 2005-2024
 load('C:\Users\palevsky\Dropbox\MATLAB\CSMBGC data processing\RCP 8_5 processed output\RCP8_5_2005to2024_ClimPOCNPPsections.mat',...
     'POCflux_all_AnnMean','NPPsum_all_AnnMean','POCflux_all_AnnMean_clim','NPPsum_all_AnnMean_clim')
 
-%% Calculate POC flux and NPP in each year at the climatological MLDmax for 2005-2024
-    TodayMLDmax_clim{1} = repmat(output(1).depthCriteria_wAnnMean_clim{4},1,1,20); %this is the climatological MLD_max for 2005-2024
-POCfluxToday_TodayMLDmax_clim = depthCriteriaVals(POCflux_all_AnnMean, z_top, TodayMLDmax_clim);
-NPPsumToday_TodayMLDmax_clim = depthCriteriaVals(NPPsum_all_AnnMean, z_top, TodayMLDmax_clim);
+%% Calculate POC flux and NPP in each year
+%at the climatological MLDmax for 2005-2024 and at the climatological MLDmax for 2081-2100
+    MLDmax_clim{1} = repmat(output(1).depthCriteria_wAnnMean_clim{4},1,1,20); %this is the climatological MLD_max for 2005-2024
+    MLDmax_clim{2} = repmat(output(2).depthCriteria_wAnnMean_clim{4},1,1,20); %this is the climatological MLD_max for 2081-2100
+
+%Evaluate today's fluxes at the climatological MLDmax for both 2005-2024 and 2018-2100
+POCfluxToday_MLDmax_clim = depthCriteriaVals(POCflux_all_AnnMean, z_top, MLDmax_clim);
+NPPsumToday_MLDmax_clim = depthCriteriaVals(NPPsum_all_AnnMean, z_top, MLDmax_clim);
 
 %% Validation of using climatological mean
 %compare POC flux from 2005-2024 evaluated at year-specific MLDmax before
@@ -17,7 +35,7 @@ NPPsumToday_TodayMLDmax_clim = depthCriteriaVals(NPPsum_all_AnnMean, z_top, Toda
     C = cmocean('balance');      
 figure(8); clf
     subplot(311)
-imagesc(rot90(mean(POCfluxToday_TodayMLDmax_clim{1},3)));
+imagesc(rot90(mean(POCfluxToday_MLDmax_clim{1},3)));
 colormap(C); colorbar;
 caxis([0 6])
 title('2005-2024 POC flux at MLD_{max}, from climatological mean MLD_{max}')
@@ -28,7 +46,7 @@ caxis([0 6])
 title('2005-2024 POC flux at MLD_{max}, from year-specific MLD_{max}')
     subplot(313)
 imagesc(rot90((POCflux_depthCriteria_AnnMean_clim{4} - ...
-mean(POCfluxToday_TodayMLDmax_clim{1},3))));
+mean(POCfluxToday_MLDmax_clim{1},3))));
 colormap(C); colorbar;
 caxis([-3 3])
 title('Difference between POC flux at MLD_{max} from year-specific MLD_{max} vs climatological mean MLD_{max}')
@@ -36,6 +54,10 @@ title('Difference between POC flux at MLD_{max} from year-specific MLD_{max} vs 
 %% Load in climatological annual mean POC flux and NPP in 3D (lat, lon, depth) for 2005-2024
 load('C:\Users\palevsky\Dropbox\MATLAB\CSMBGC data processing\RCP 8_5 processed output\RCP8_5_2081to2100_ClimPOCNPPsections.mat',...
     'POCflux_all_AnnMean','NPPsum_all_AnnMean','POCflux_all_AnnMean_clim','NPPsum_all_AnnMean_clim')
+
+%% Calculate POC flux and NPP in each year from 2081-2100 at the climatological MLDmax for 2005-2024 and for 2081-2100
+POCfluxEndCent_MLDmax_clim = depthCriteriaVals(POCflux_all_AnnMean, z_top, MLDmax_clim);
+NPPsumEndCent_MLDmax_clim = depthCriteriaVals(NPPsum_all_AnnMean, z_top, MLDmax_clim);
 
 %% Calculate mean POC depth profile for Taylor decomposition
 %Reshape as list of depth profiles in all grid cells
@@ -109,30 +131,97 @@ j = 5; %100 m - calculate baseline values at 100 m and then add in dMLD/dt term 
     Taylor_eRatioTerm_MLDmax = deRatiodt_MLDmax.*(output(1).NPPsum_depthCriteria_AnnMean_clim{4});
     Taylor_Residual_100m = dEPdt - Taylor_NPPTerm - Taylor_eRatioTerm;
     Taylor_Residual_MLDmax = dEPdt_MLDmax - Taylor_NPPTerm - Taylor_eRatioTerm_MLDmax; %Scott suggested adding Taylor_MLDchange, but residual still doesn't work out right
-    Taylor_Residual_MLDmax_wMLDchange = dEPdt_MLDmax - Taylor_NPPTerm - Taylor_eRatioTerm - Taylor_MLDchange; 
+    Taylor_Residual_MLDmax_wMLDchange = dEPdt_MLDmax - Taylor_NPPTerm - Taylor_eRatioTerm - Taylor_MLDchange;
+
+%Calculate Taylor decomposition for MLD_max using climatological MLD_max
+%values from beginning and end of century as extra depth criteria
+dNPPdt_MLDmaxBegCent = mean(NPPsumEndCent_MLDmax_clim{1},3) - mean(NPPsumToday_MLDmax_clim{1},3);
+eRatio_MLDmaxBegCent = mean((POCfluxToday_MLDmax_clim{1}./NPPsumToday_MLDmax_clim{1}),3);
+Taylor_NPPTerm_MLDmaxBegCent = dNPPdt_MLDmaxBegCent.*eRatio_MLDmaxBegCent;
+
+dERatiodt_MLDmaxBegCent = mean((POCfluxEndCent_MLDmax_clim{1}./NPPsumEndCent_MLDmax_clim{1}),3) - ...
+    mean((POCfluxToday_MLDmax_clim{1}./NPPsumToday_MLDmax_clim{1}),3);
+NPP_MLDmaxBegCent = mean(NPPsumToday_MLDmax_clim{1},3);
+Taylor_eRatioTerm_MLDmaxBegCent = dERatiodt_MLDmaxBegCent.*NPP_MLDmaxBegCent;
+
+%Calculated with climatological mean MLD_max depth horizons for both
+%beginning and end of century
+    %Taylor_dEPdt_MLDmaxChange = mean(POCfluxEndCent_MLDmax_clim{2},3) - mean(POCfluxEndCent_MLDmax_clim{1},3); 
+%Calculated with climatological MLD_max depth horizon from beginning of
+%century, but depth-specific values for end of century - BETTER METHOD
+Taylor_dEPdt_MLDmaxChange = output(2).POCflux_depthCriteria_AnnMean_clim{4} - mean(POCfluxEndCent_MLDmax_clim{1},3);
+
+Taylor_Residual_MLDmax_BegVsEndCentCriteria = dEPdt_MLDmax - Taylor_NPPTerm_MLDmaxBegCent -...
+    Taylor_eRatioTerm_MLDmaxBegCent - Taylor_dEPdt_MLDmaxChange;
     
+%% Visualize Taylor Decomposition terms for the two different ways of adding a 3rd term in the MLD_max decomposition
+
     figure(fignum); clf; fignum = fignum + 1;
-        cmin = -100; cmax = 100;
+        cmin = -1.5; cmax = 1.5;
         C = cmocean('balance');
         j = 4; %For MLD_max
     subplot(511)
-        imagesc(rot90(100*dEPdt_MLDmax./output(1).POCflux_depthCriteria_AnnMean_clim{j})); colormap(C); colorbar;
+        imagesc(rot90(dEPdt_MLDmax)); colormap(C); colorbar;
         caxis([cmin cmax])
-        title('Percent change in POC flux, from 2005-2024 to 2081-2100')
+        title('Change in POC flux at MLD_{max}, from 2005-2024 to 2081-2100')
     subplot(512)
-        imagesc(rot90(100*Taylor_NPPTerm./output(1).POCflux_depthCriteria_AnnMean_clim{j})); colormap(C); colorbar;
+        imagesc(rot90(Taylor_NPPTerm_MLDmaxBegCent)); colormap(C); colorbar;
         caxis([cmin cmax])
-        title('Percent change due to NPP (\deltaNPP/\deltat x e-ratio)')
+        title('Change due to NPP at MLD_{max, 2005-2024} (\deltaNPP/\deltat x e-ratio)')
     subplot(513)
-        imagesc(rot90(100*Taylor_eRatioTerm_MLDmax./output(1).POCflux_depthCriteria_AnnMean_clim{j})); colormap(C); colorbar;
+        imagesc(rot90(Taylor_eRatioTerm_MLDmaxBegCent)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Change due to e-ratio at MLD_{max, 2005-2024} (\deltae-ratio/\deltat x NPP)')
+    subplot(514)
+        imagesc(rot90(Taylor_dEPdt_MLDmaxChange)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Change due to change in MLD_{max} depth criterion (\deltaPOC flux/\deltaz x \deltaMLD_{max})')
+    subplot(515)
+        imagesc(rot90(Taylor_Residual_MLDmax_BegVsEndCentCriteria)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Residual of 1st order Taylor decomposition above')
+
+% Visualize 2nd method of taylor decomposition w/ beginning and end of 21st century-specific MLD_max horizons        
+    figure(fignum); clf; fignum = fignum + 1;
+        cmin = -1.5; cmax = 1.5;
+        C = cmocean('balance');
+        j = 4; %For MLD_max
+    subplot(511)
+        imagesc(rot90(dEPdt_MLDmax)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Change in POC flux, from 2005-2024 to 2081-2100')
+    subplot(512)
+        imagesc(rot90(Taylor_NPPTerm)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Change due to NPP (\deltaNPP/\deltat x e-ratio)')
+    subplot(513)
+        imagesc(rot90(Taylor_eRatioTerm_MLDmax)); colormap(C); colorbar;
         caxis([cmin cmax])
         title('Percent change due to e-ratio (\deltae-ratio/\deltat x NPP)')
     subplot(514)
-        imagesc(rot90(100*Taylor_MLDchange./output(1).POCflux_depthCriteria_AnnMean_clim{j})); colormap(C); colorbar;
+        imagesc(rot90(Taylor_MLDchange)); colormap(C); colorbar;
         caxis([cmin cmax])
-        title('Percent change due to MLD_{max} (\deltaPOC flux/\deltaz x \deltaMLD_{max})')
+        title('Change due to MLD_{max} (\deltaPOC flux/\deltaz x \deltaMLD_{max})')
     subplot(515)
-        imagesc(rot90(100*Taylor_Residual_MLDmax./output(1).POCflux_depthCriteria_AnnMean_clim{j})); colormap(C); colorbar;
+        imagesc(rot90(Taylor_Residual_MLDmax)); colormap(C); colorbar;
         caxis([cmin cmax])
-        title('Residual of 1st order Taylor decomposition above (%)')
+        title('Residual of 1st order Taylor decomposition above')
+
+% Compare the residuals with the total change
+   figure(fignum); clf; fignum = fignum + 1;
+   cmin = -1.5; cmax = 1.5;
+       subplot(311)
+        imagesc(rot90(dEPdt_MLDmax)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Total dEPdt at MLD_max')
+        
+      subplot(312)
+        imagesc(rot90(Taylor_Residual_MLDmax_BegVsEndCentCriteria)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Residual of 1st order Taylor decomposition - new method')
+        
+      subplot(313)
+        imagesc(rot90(Taylor_Residual_MLDmax)); colormap(C); colorbar;
+        caxis([cmin cmax])
+        title('Residual of 1st order Taylor decomposition - old method')
     
